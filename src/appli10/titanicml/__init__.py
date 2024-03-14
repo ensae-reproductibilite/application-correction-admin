eff2a9fb332a76efcaf7cdf9210536e715ed8b28
linogaliana <lino.galiana@insee.fr>
2024-03-14 16:43:19 +0000

    updae

--- src/appli10/titanicml/__init__.py
+++ src/appli10/titanicml/__init__.py
@@ -1,21 +1,17 @@
 from .data.import_data import (
-    import_data, import_yaml_config
+    import_yaml_config,
+    split_and_count
 )
-from .features.build_features import (
-    create_variable_title,
-    fill_na_titanic,
-    label_encoder_titanic,
-    check_has_cabin,
-    ticket_length
+from .pipeline.build_pipeline import (
+    split_train_test,
+    create_pipeline
 )
-from .models.train_evaluate import random_forest_titanic
-
+from .models.train_evaluate import (
+    evaluate_model
+)
 __all__ = [
-    "import_data", "import_yaml_config",
-    "create_variable_title",
-    "fill_na_titanic",
-    "label_encoder_titanic",
-    "check_has_cabin",
-    "ticket_length",
-    "random_forest_titanic"
+    "import_yaml_config",
+    "split_and_count",
+    "create_pipeline",
+    "evaluate_model"
 ]
