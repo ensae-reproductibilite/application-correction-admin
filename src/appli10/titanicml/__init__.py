eeb4bf113b88f35720134d72e0d0974457560e86
lgaliana <lino.galiana@insee.fr>
2025-01-29 10:43:08 +0000

    update package

--- src/appli10/titanicml/__init__.py
+++ src/appli10/titanicml/__init__.py
@@ -1,5 +1,5 @@
 from .data.import_data import (
-    import_yaml_config
+    split_and_count
 )
 from .pipeline.build_pipeline import (
     split_train_test,
@@ -9,7 +9,8 @@ from .models.train_evaluate import (
     evaluate_model
 )
 __all__ = [
-    "import_yaml_config",
+    "split_and_count",
+    "split_train_test",
     "create_pipeline",
     "evaluate_model"
 ]
