49ce07dfb9cc19c96ebcc39ea0a863a2e1a14dac
lgaliana <lino.galiana@insee.fr>
2026-02-01 16:04:46 +0000

    appli10

--- src/appli10/docs/main.py
+++ src/appli10/docs/main.py
@@ -5,21 +5,29 @@ Prediction de la survie d'un individu sur le Titanic
 import os
 from dotenv import load_dotenv
 import argparse
-from loguru import logger
+import logging
 
-import pathlib
 import pandas as pd
+import duckdb
 
 from sklearn.model_selection import train_test_split
+from titanicml.pipeline.build_pipeline import create_pipeline
+from titanicml.models.train_evaluate import evaluate_model
 
-from titanicml import (
-    create_pipeline,
-    evaluate_model
+
+logging.basicConfig(
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M",
+    level=logging.DEBUG,
+    handlers=[
+        logging.FileHandler("recording.log"),
+        logging.StreamHandler()
+    ]
 )
 
 # ENVIRONMENT CONFIGURATION ---------------------------
 
-logger.add("recording.log", rotation="500 MB")
 load_dotenv()
 
 parser = argparse.ArgumentParser(description="Paramètres du random forest")
@@ -28,28 +36,28 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-URL_RAW = "https://minio.lab.sspcloud.fr/lgaliana/ensae-reproductibilite/data/raw/data.csv"
+MAX_DEPTH = None
+MAX_FEATURES = "sqrt"
+URL_RAW = "https://minio.lab.sspcloud.fr/lgaliana/ensae-reproductibilite/data/raw/data.parquet"
 
 n_trees = args.n_trees
 jeton_api = os.environ.get("JETON_API", "")
-data_path = os.environ.get("data_path", URL_RAW)
-data_train_path = os.environ.get("train_path", "data/derived/train.csv")
-data_test_path = os.environ.get("test_path", "data/derived/test.csv")
-MAX_DEPTH = None
-MAX_FEATURES = "sqrt"
+data_path = os.environ.get("DATA_PATH", URL_RAW)
+
+con = duckdb.connect(database=":memory:")
 
 if jeton_api.startswith("$"):
-    logger.info("API token has been configured properly")
+    logging.info("API token has been configured properly")
 else:
-    logger.warning("API token has not been configured")
+    logging.warning("API token has not been configured")
 
 
 # IMPORT ET STRUCTURATION DONNEES --------------------------------
 
-p = pathlib.Path("data/derived/")
-p.mkdir(parents=True, exist_ok=True)
-
-TrainingData = pd.read_csv(data_path)
+query_definition = f"SELECT * FROM read_parquet('{data_path}')"
+TrainingData = con.sql(
+    query_definition
+).to_df()
 
 y = TrainingData["Survived"]
 X = TrainingData.drop("Survived", axis="columns")
@@ -57,15 +65,10 @@ X = TrainingData.drop("Survived", axis="columns")
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.1
 )
-pd.concat([X_train, y_train], axis = 1).to_parquet(data_train_path)
-pd.concat([X_test, y_test], axis = 1).to_parquet(data_test_path)
-
 
 
 # PIPELINE ----------------------------
 
-
-# Create the pipeline
 pipe = create_pipeline(
     n_trees, max_depth=MAX_DEPTH, max_features=MAX_FEATURES
 )
@@ -74,12 +77,9 @@ pipe = create_pipeline(
 # ESTIMATION ET EVALUATION ----------------------
 
 pipe.fit(X_train, y_train)
-
-
-# Evaluate the model
 score, matrix = evaluate_model(pipe, X_test, y_test)
 
-logger.success(f"{score:.1%} de bonnes réponses sur les données de test pour validation")
-logger.debug(20 * "-")
-logger.info("Matrice de confusion")
-logger.debug(matrix)
+logging.info(f"{score:.1%} de bonnes réponses sur les données de test pour validation")
+logging.info(20 * "-")
+logging.info("matrice de confusion")
+logging.info(matrix)
